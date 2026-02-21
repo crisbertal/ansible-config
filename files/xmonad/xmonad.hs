e0a309f04facb20a602b30854a429ab536d0d80e
Cristobal Bergillos <cristobalbergillosnavarro@gmail.com>
2026-02-21 12:19:17 +0100

    Xmonad: three col layout

--- files/xmonad/xmonad.hs
+++ files/xmonad/xmonad.hs
@@ -2,10 +2,20 @@ import XMonad
 
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import XMonad.Layout.ThreeColumns
+
+myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = ThreeColMid nmaster delta ratio
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1
+    ratio    = 1/2
+    delta    = 3/100
 
 main :: IO ()
-main = xmonad $ def
-	{ modMas = mod4Mask -- Mod is Super key
+main = xmonad $ def   
+	{ modMask    = mod4Mask     -- Mod is Super key
+	, layoutHook = myLayout     -- Custom Layout
 	}
         `additionalKeysP`
         [ ("M-b", spawn "firefox" )
